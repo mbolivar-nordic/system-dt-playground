5ab696ea1c752d109f077e98141f822286659801
Martí Bolívar <marti.bolivar@nordicsemi.no>
2022-04-07 11:41:38 -0700

    update run.sh to run in container

--- run.sh
+++ run.sh
@@ -8,5 +8,4 @@ cd "$HERE"
 
 set -x
 
-lopper.py -f -O. -i lop-cpuapp.dts nrf5340_system.dts out-cpuapp.dts
-lopper.py -f -O. -i lop-cpunet.dts nrf5340_system.dts out-cpunet.dts
+/root/lopper/lopper.py -f --enhanced -i lops-zephyr-secure.dts nrf5340_system.dts sdt.dts
